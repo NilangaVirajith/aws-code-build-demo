90fb1cb6f7495b86ef70a0a02ef166f6b526f479
NilangaVirajith <nilangavirajith5@gmail.com>
2020-07-22 21:26:30 +0530

    adde people endpoint

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -5,12 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class PersonController {
 
     @GetMapping("/person/{name}")
     public Person getPerson(@PathVariable("name") String name) {
         return new Person(name == null ? "Anonymous" : name, "Sri Lanka", "Male");
+    }
+
+    @GetMapping("/people")
+    public List<Person> getPeople() {
+
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Nilanga", "Sri Lanka", "Male"));
+        people.add(new Person("Shash", "Sri Lanka", "Female"));
+        people.add(new Person("Virajith", "Sri Lanka", "Male"));
+
+        return people;
     }
 
 }
